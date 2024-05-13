d60f8984a75c4ddeb82393a952708781b2314448
TeraSeo <seotj0413@gmail.com>
2024-05-13 14:03:52 +0900

    Initial Commit

---
+++ Ios_Assignment3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ios_Assignment3
+//
+//  Created by 서태준 on 5/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
